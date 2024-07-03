5e6813e5ef25b8400ffe50308ad501ca6e3c8c53
Liam Bolan <AQ231688@aquinas.ac.uk>
2024-07-03 17:06:08 +0100

    e

--- Roman numerals/Program.cs
+++ Roman numerals/Program.cs
@@ -10,71 +10,88 @@ namespace Roman_numerals
     {
         static void Main(string[] args)
         {
-            string[] romanchars = { "M", "D", "C", "L", "X", "V", "I" };
-            int m = 0;
-            int d = 0;
-            int c = 0;
-            int l = 0;
-            int x = 0;
-            int v = 0;
-            int i2 = 0;
-            
-            for (int i = 0; i < romanchars.Length; i++)
-            {
-                if(romanchars[i] == "M")
-                {
-                    m = 1000;
-                }
-                if(romanchars[i] == "D")
-                {
-                    d = 500;
-                }
-                if (romanchars[i] == "C")
-                {
-                    c = 100;
-                }
-                if (romanchars[i] == "L")
-                {
-                    l = 50;
-                }
-                if (romanchars[i] == "X")
-                {
-                    x = 10;
-                }
-                if (romanchars[i] == "V")
-                {
-                    v = 5;
-                }
-                if (romanchars[i] == "I")
-                {
-                    i2 = 1;
-                }
-            }
-
-
             string romanstring;
-            
-            int romannum;
+            int romannum = 0;
 
             Console.Write("Enter a roman numeral: ");
             romanstring = Console.ReadLine();
             romanstring = romanstring.ToUpper();
 
-            for (int i = 0;i < romanstring.Length; i++)
+
+
+            for (int i = 0; i < romanstring.Length; i++)
             {
-                for (int j = 0; j < romanchars.Length; j++)
+                if (i < romanstring.Length-1)
                 {
-                    if (romanstring[i].ToString() == romanchars[j])
+                    
+                    if (romanstring[i] == 'I' && romanstring[i + 1] == 'V')
                     {
-                        
+                        romannum -= 6;
+                        romannum += 4;
+                    }
+                    if (romanstring[i] == 'I' && romanstring[i + 1] == 'X')
+                    {
+                        romannum -= 11;
+                        romannum += 9;
+                    }
+                    if (romanstring[i] == 'X' && romanstring[i + 1] == 'L')
+                    {
+                        romannum -= 60;
+                        romannum += 40;
+                    }
+                    if (romanstring[i] == 'X' && romanstring[i + 1] == 'C')
+                    {
+                        romannum -= 110;
+                        romannum += 90;
+                    }
+                    if (romanstring[i] == 'C' && romanstring[i + 1] == 'D')
+                    {
+                        romannum -= 600;
+                        romannum += 400;
+                    }
+                    if (romanstring[i] == 'C' && romanstring[i + 1] == 'M')
+                    {
+                        romannum -= 1100;
+                        romannum += 900;
                     }
                 }
+                
+                if (romanstring[i] == 'M')
+                {
+                    romannum += 1000;
+                }
+                if (romanstring[i] == 'D')
+                {
+                    romannum += 500;
+                }
+                if (romanstring[i] == 'C')
+                {
+                    romannum += 100;
+                }
+                if (romanstring[i] == 'L')
+                {
+                    romannum += 50;
+                }
+                if (romanstring[i] == 'X')
+                {
+                    romannum += 10;
+                }
+                if (romanstring[i] == 'V')
+                {
+                    romannum += 5;
+                }
+                if (romanstring[i] == 'I')
+               {
+                    romannum += 1;
+                }
             }
-
-            Console.WriteLine($"\nRoman numeral version = {romanstring}");
-            Console.ReadLine();
-
-
+                Console.WriteLine($"\nRoman numeral version = {romanstring}");
+                Console.WriteLine($"\nRoman number is {romannum}");
+                Console.ReadLine();
+            
         }
     }
 }
+            
+    
+
